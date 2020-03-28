044ac56ec884058e1d3b4608f7ce598530168e21
myrul deen <funstudy10@gmail.com>
2020-03-29 02:57:25 +0800

    add some comment

--- web-blocker.py
+++ web-blocker.py
@@ -5,25 +5,36 @@ hosts_temp = "hosts"
 # hosts_path = "/etc/hosts" # For Linux or Mac
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts" # For Windows
 redirect = "127.0.0.1"
-website_list = ["www.facebook.com", "www.youtube.com", "www.twitter.com"]
+website_list = ["www.facebook.com", "www.youtube.com", "www.twitter.com"] # List of website to block
 
-while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day,9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,18):
-        print("Working hours...")
-        with open(hosts_path, 'r+') as file:
-            content = file.read()
-            for website in website_list:
-                if website in content:
-                    pass
-                else:
-                    file.write(redirect+" "+ website+"\n")
-    else:
-        with open(hosts_path, 'r+') as file:
-            content = file.readlines()
-            file.seek(0)
-            for line in content:
-                if not any(website in line for website in website_list):
-                    file.write(line)
-            file.truncate()
-        print("Break hours...")
-    time.sleep(5)      
+def main():
+    while True:
+        # Check working hours example: 9:00 pm to 5:00 pm 
+        if dt(dt.now().year, dt.now().month, dt.now().day,9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,18):
+            print("Wroking hours...")
+            # Open file and set permission to read/write
+            with open(hosts_path, 'r+') as file:
+                # Read file content
+                content = file.read()
+                for website in website_list:
+                    if website in content:
+                        pass
+                    else:
+                        # Write/append to file
+                        file.write(redirect+" "+ website+"\n")
+        else:
+            with open(hosts_path, 'r+') as file:
+                # Read file line by line
+                content = file.readlines()
+                file.seek(0)
+                for line in content:
+                    if not any(website in line for website in website_list):
+                        file.write(line)
+                file.truncate()
+            print("Break hours...")
+        time.sleep(5)      
+
+
+if __name__ == "__main__":
+    main()
+
